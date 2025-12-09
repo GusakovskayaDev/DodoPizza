79d6e581e96575cb699f6eb6a7595d4a702bfc3b
GusakovskayaDev <gusakovskayadev@gmail.com>
2025-12-09 14:06:31 +0300

    Add ingredient route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,7 +5,7 @@ if (!process.env.DATABASE_URL) {
   throw new Error('❌ DATABASE_URL не задан в .env');
 }
 
-export const prisma = new PrismaClient({
+const prisma = new PrismaClient({
   datasourceUrl: process.env.DATABASE_URL,
 });
 
