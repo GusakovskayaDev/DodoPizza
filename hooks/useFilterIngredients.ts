1c8d9ee79420f0a2f463067b7e3d2c56b93fb846
GusakovskayaDev <gusakovskayadev@gmail.com>
2025-12-09 22:46:52 +0300

    Add useFilterIngredients hook

---
+++ hooks/useFilterIngredients.ts
@@ -0,0 +1,33 @@
+import { Api } from "@/services/api-client";
+import { Ingredient } from "@prisma/client";
+import { useEffect, useState } from "react";
+
+type IngredientItem = Pick<Ingredient, 'id' | 'name'>;
+
+interface ReturnProps {
+  ingredients: IngredientItem[];
+  loading: boolean;
+}
+
+export const useFilterIngredients = (): ReturnProps => {
+  const [ingredients, setIngredients] = useState<ReturnProps['ingredients']>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchIngredients(){
+      try{
+        setLoading(true);
+        const ingredients = await Api.ingredients.getAll();
+        setIngredients(ingredients.map((ingredient) => ({ id: ingredient.id, name: ingredient.name})));
+      } catch(error){
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchIngredients();
+  }, []);
+
+  return { ingredients, loading };
+}
