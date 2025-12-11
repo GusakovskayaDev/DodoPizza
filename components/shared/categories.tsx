8092e46df473f363053f1139b85345874dac2639
GusakovskayaDev <gusakovskayadev@gmail.com>
2025-12-11 15:18:25 +0300

    fix: correct TopBar display

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -2,29 +2,20 @@
 
 import { cn } from "@/lib/utils";
 import { useCategoryStore } from "@/store/category";
+import { Category } from "@prisma/client";
 import React from "react";
 
 interface Props {
+  items: Category[];
   className?: string,
 }
 
-const cats = [
-  {id: 1, name: 'Пиццы'},
-  {id: 2, name: 'Комбо'},
-  {id: 3, name: 'Закуски'},
-  {id: 4, name: 'Коктейли'},
-  {id: 5, name: 'Кофе'},
-  {id: 6, name: 'Напитки'},
-  {id: 7, name: 'Десерты'},
-  {id: 8, name: 'Еще десерты'},
-]
-
-export const Categories: React.FC<Props> = ({ className }) => {
+export const Categories: React.FC<Props> = ({ items, className }) => {
   const categoryActiveId = useCategoryStore((state) => state.activeId)
   return (
     <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
       {
-        cats.map(({id, name}, index) => (
+        items.map(({id, name}, index) => (
           <a className={cn(
             'flex items-center fonr-bold h-11 rounded-2xl px-5',
             categoryActiveId === id && 'bg-white shadow-md shadow-gray-200 text-primary cursor'
