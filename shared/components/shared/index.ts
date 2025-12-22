2a261a10bc3cfca7d1f6fe34f042e471812aa5e6
GusakovskayaDev <gusakovskayadev@gmail.com>
2025-12-22 15:03:21 +0300

    feat: add cart drawer

--- shared/components/shared/index.ts
+++ shared/components/shared/index.ts
@@ -14,4 +14,6 @@ export { ChooseProductForm } from './choose-product-form';
 export { ChoosePizzaForm } from './choose-pizza-form';
 export { ProductsGroupList } from './products-group-list';
 export { IngredientItem } from './ingredient-item';
+export { CartButton } from './cart-button';
+export { CartDrawer } from './cart-drawer';
 export * from './modals';
