6de4a9caca946e91e672fa7b1edd4e417e6b1d8e
GusakovskayaDev <gusakovskayadev@gmail.com>
2025-12-23 17:31:46 +0300

    feat: add PATCH endpoint to update cart item quantity

---
+++ app/api/cart/[id]/route.ts
@@ -0,0 +1,41 @@
+import { updateCartTotalAmount } from "@/shared/lib";
+import { prisma } from "@/shared/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(req: NextRequest, { params }: { params: { id: string }}) {
+  try {
+    const id = Number((await params).id);
+    const data = (await req.json()) as { quantity: number };
+    const token = req.cookies.get('cartToken')?.value;
+
+    if(!token){
+      return NextResponse.json({ error: 'Cart token not found'});
+    }
+
+    const cartItem = await prisma.cartItem.findFirst({
+      where: {
+        id,
+      }
+    });
+
+    if(!cartItem){
+      return NextResponse.json({ error: 'Cart item not found'});
+    }
+
+    await prisma.cartItem.update({
+      where: {
+        id,
+      },
+      data: {
+        quantity: Number(data.quantity),
+      }
+    });
+
+    const updatedUserCart = await updateCartTotalAmount(token);
+
+    return NextResponse.json(updatedUserCart);
+  } catch(error){
+    console.log('[CART_PATCH] Server error', error);
+    return NextResponse.json({message: 'Не удалось обновить корзину'}, {status: 500});
+  }
+}
